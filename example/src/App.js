157b9efa2585ae52f4e4c5453e9a2af080b272aa
Shahidul Islam Majumder <shahidul.islam@springrainit.com>
2019-07-11 12:32:03 +0600

    :tada: Add Text component

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'tookri'
+import { Text } from 'tookri';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Text variant="h4" color="primary">
+          Hello there...
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          iusto, dignissimos excepturi culpa consequatur aliquid accusamus odit
+          omnis sunt similique cupiditate consequuntur soluta, iure molestiae,
+          nesciunt neque! Ex, ratione, aliquid?
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          iusto, dignissimos excepturi culpa consequatur aliquid accusamus odit
+          omnis sunt similique cupiditate consequuntur soluta, iure molestiae,
+          nesciunt neque! Ex, ratione, aliquid?
+        </Text>
       </div>
-    )
+    );
   }
 }
